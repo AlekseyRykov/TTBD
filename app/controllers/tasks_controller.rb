d4725e13ad3a4d812b74258ef843ff3540334f7e
Aleksey Rykov <a.rykov8923@gmail.com>
2018-11-16 21:37:32 +1100

    Feature
    add validation for projects and tasks
    add output on project page for task list and task form
    add partials for task list output and task form
    edit tasks_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,21 +1,22 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_project, only: [:create, :destroy]
 
   # GET /tasks
   # GET /tasks.json
-  def index
-    @tasks = Task.all
-  end
+  # def index
+  #   @tasks = Task.all
+  # end
 
   # GET /tasks/1
   # GET /tasks/1.json
-  def show
-  end
+  # def show
+  # end
 
   # GET /tasks/new
-  def new
-    @task = Task.new
-  end
+  # def new
+  #   @task = Task.new
+  # end
 
   # GET /tasks/1/edit
   def edit
@@ -24,15 +25,15 @@ class TasksController < ApplicationController
   # POST /tasks
   # POST /tasks.json
   def create
-    @task = Task.new(task_params)
-
+    # @task = Task.new(task_params)
+    @task = @project.tasks.new(task_params)
     respond_to do |format|
       if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
-        format.json { render :show, status: :created, location: @task }
+        format.html { redirect_to @project, notice: 'Task was successfully created.' }
+        # format.json { render :show, status: :created, location: @task }
       else
-        format.html { render :new }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
+        format.html { render "projects/show" }
+        # format.json { render json: @task.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -54,10 +55,11 @@ class TasksController < ApplicationController
   # DELETE /tasks/1
   # DELETE /tasks/1.json
   def destroy
+    @task = @project.tasks.find(params[:id])
     @task.destroy
     respond_to do |format|
-      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html { redirect_to @project, notice: 'Task was successfully destroyed.' }
+      # format.json { head :no_content }
     end
   end
 
@@ -65,6 +67,9 @@ class TasksController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_task
       @task = Task.find(params[:id])
+    end
+    def set_project
+      @project = Project.find(params[:project_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
