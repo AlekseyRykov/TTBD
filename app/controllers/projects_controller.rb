8e5332512646250f156941c700b2990056d302c7
Aleksey Rykov <a.rykov8923@gmail.com>
2018-12-05 15:54:34 +1100

    Refactor
    remove priority from projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -72,6 +72,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:title, :priority)
+      params.require(:project).permit(:title)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@ ActiveRecord::Schema.define(version: 2018_11_16_030620) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
-    t.integer "priority"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
